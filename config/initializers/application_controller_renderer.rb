af6508331ec0b7c64c6c3d8a981867c988ee82b9
k.taniguchi <k.taniguchi@cybermissions.co.jp>
2021-10-15 16:14:44 +0900

    編集機能を追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '39eca3946dd67cbceaf22168b33f94fa4d7f090317ae68c4625afcbf6cca6adee27b7e203caafeeac6662f5b4edc6f15b56ea104cfff05313e2e4b2ac38ccf88'
